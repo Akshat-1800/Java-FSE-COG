cd690131862d451d8f72060f3f8d4b125607a73f
Akshat-1800 <jaiswalakshat874@gmail.com>
2026-07-06 00:19:48 +0530

    Completed Excercise 2 of Week 3 Spring REST by changing the project spring-learn

--- Week_3/spring-learn/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week_3/spring-learn/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,9 +1,14 @@
-package com.cognizant.spring_learn;
+package com.cognizant.springlearn;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -18,5 +23,17 @@ public class SpringLearnApplication {
 		SpringApplication.run(SpringLearnApplication.class, args);
 
 		LOGGER.info("END");
+	}
+	public static void displayDate() throws Exception {
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("date-format.xml");
+
+		SimpleDateFormat format =
+				context.getBean("dateFormat", SimpleDateFormat.class);
+
+		Date date = format.parse("31/12/2018");
+
+		System.out.println(date);
 	}
 }
